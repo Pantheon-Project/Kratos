b19ac6aab509df67c9dbbabe423bc101c17327d8
LswaN58 <lwswanson2@wisc.edu>
2023-01-21 11:48:52 -0600

    Allow getting Set from Dict, and add getters.

--- app/model/Set.js
+++ app/model/Set.js
@@ -9,7 +9,23 @@ export default class ExerciseSet {
    constructor(reps, weight, RPE, RIR=null) {
       this.reps = reps
       this.weight = weight;
-      this.RPE = RPE;
-      this.RIR = RIR || 10 - Math.ceil(RPE);
+      this.rpe = RPE;
+      this.rir = RIR != null ? RIR : 10 - Math.ceil(RPE);
    }
+
+   static FromDict(set_dict) {
+      reps = parseInt(set_dict.reps)
+      this.reps = reps != NaN ? reps : 0
+      weight = parseInt(set_dict.weight)
+      this.weight = weight != NaN ? weight : 0
+      rpe = parseInt(set_dict.rpe)
+      this.rpe = rpe != NaN ? rpe : 0
+      rir = parseInt(set_dict.rir)
+      this.rir = rir != NaN ? rir : 0
+   }
+
+   get Repetitions() { return this.reps; }
+   get Weight()      { return this.weight; }
+   get RPE()         { return this.rpe; }
+   get RIR()         { return this.rir; }
 }
